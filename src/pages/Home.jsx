e050bc53177b9ca7dc287def1f2959e96f0bc01a
Jeremy Montes <jeremymontes@pursuit.org>
2023-11-20 09:40:23 -0500

    deployed online

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,9 @@ export default function Home(){
         <div className='homePrompt'>
 
             <h1> Welcome to The Grid</h1>
-
+<p> This task management app allows you to keep track of those important tasks.
+    Click the 'View All' button to see what you have in store for the days to come or the 'New Task' button to keep track of more tasks.
+</p>
             <br />
 
            <Link to='/grid'> <button>View All</button></Link>
